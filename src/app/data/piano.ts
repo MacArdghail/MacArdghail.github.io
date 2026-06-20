daf649e19fef00d1a988ae66ab2b4e7931d9f867
Fionn McArdle <f.mcardle1@universityofgalway.ie>
2026-06-20 21:27:33 +0100

    [23] - Comment out filler for now

--- src/app/data/piano.ts
+++ src/app/data/piano.ts
@@ -10,15 +10,15 @@ export type PianoPerformance = {
 };
 
 export const pianoPerformances: PianoPerformance[] = [
-  {
-    slug: 'cinema-paradiso-love-theme',
-    index: 1,
-    title: 'Cinema Paradiso - Love Theme',
-    youtubeUrl: 'https://www.youtube.com/watch?v=op483l5s_dQ',
-    youtubeEmbedId: 'op483l5s_dQ',
-    description:
-      'Ennio Morricone\'s love theme from Cinema Paradiso. A beautiful song from my favourite film - This is filler, I will record myself',
-    tags: ['[Morricone]', '[Cinema Paradiso]', '[2026]'],
-    year: 2024,
-  },
+//   {
+//     slug: 'cinema-paradiso-love-theme',
+//     index: 1,
+//     title: 'Cinema Paradiso - Love Theme',
+//     youtubeUrl: 'https://www.youtube.com/watch?v=op483l5s_dQ',
+//     youtubeEmbedId: 'op483l5s_dQ',
+//     description:
+//       'Ennio Morricone\'s love theme from Cinema Paradiso. A beautiful song from my favourite film - This is filler, I will record myself',
+//     tags: ['[Morricone]', '[Cinema Paradiso]', '[2026]'],
+//     year: 2024,
+//   },
 ];
--- src/app/data/posts.ts
+++ src/app/data/posts.ts
@@ -9,14 +9,14 @@ export type Post = {
 };
 
 export const posts: Post[] = [
-  {
-    slug: 'post-one',
-    index: 1,
-    title: 'Blog Post 1',
-    image: 'https://placehold.co/600x300/1a1a1a/f0eeea?text=Post+One',
-    description:
-      'This is just filler to see how it will look when I eventually add blog posts',
-    tags: ['[essay]', '[stuff]', '[2026]'],
-    year: 2025,
-  },
+//   {
+//     slug: 'post-one',
+//     index: 1,
+//     title: 'Blog Post 1',
+//     image: 'https://placehold.co/600x300/1a1a1a/f0eeea?text=Post+One',
+//     description:
+//       'This is just filler to see how it will look when I eventually add blog posts',
+//     tags: ['[essay]', '[stuff]', '[2026]'],
+//     year: 2025,
+//   },
 ];
